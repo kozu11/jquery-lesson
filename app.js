38187d96f506e08b3ace3ba914f3cebd04f311ca
kozu11 <nk11.kzdl20@gmail.com>
2021-12-03 10:43:35 +0900

    [Finish]javascript7章

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-// $(function(){
-//   $('.box1').mouseover(function(){
-//     $('.box1').css({'background-color': '#0000ff'});
-//   });
-//   $('.box1').mouseout(function(){
-//     $('.box1').css({'background-color': '#ff0000'});
-//   });
-// });
-
-// $(function(){
-//   $('.box1').mouseover(function(){
-//     $('.box1').addClass('box-ext');
-//   });
-//   $('.box1').mouseout(function(){
-//     $('.box1').removeClass('box-ext');
-//   });
-// });
-
 $(function(){
   $('.box1').on('click', function(){
-    $('.box1').addClass('box-ext');
-  })
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box-ext');
+    $(this).slideUp();
+  });
+});
+
+
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color', 'red');
   });
 });
